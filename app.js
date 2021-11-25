3016fffa2d37bb5cd25483b86583996f0b1a2178
Bharatrathod-81 <Bharat21@navgurukul.org>
2021-11-25 17:06:21 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate=document.querySelector("#btn-translate");
+var txtIput=document.querySelector("#txt-input");
+console.log(txtIput)
 function clickHandker(){
     console.log("clicked")
+    console.log("input",txtIput.value);
+    
 }
-btnTranslate.addEventListener("click",clickHandker());
+btnTranslate.addEventListener("click",clickHandker);
