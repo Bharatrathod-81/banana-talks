11fe1be978b412ca500c5191789470254bf20d1b
Bharatrathod-81 <Bharat21@navgurukul.org>
2021-11-27 10:22:26 +0530

    banana talks done

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtIput=document.querySelector("#txt-input");
 var outPut=document.querySelector("#output");
-console.log(txtIput)
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(serverURL){
+    return serverURL+"?"+"text= "+txtIput.value
+}
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server! Try again after some time")
+}
+// var inputText=txtIput.value;
 function clickHandker(){
-    outPut.innerText="ajajakaka "+txtIput.value;
+    fetch(getTranslationURL(serverURL))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        outPut.innerText=translatedText
+      })
+    .catch(errorHandler)
 }
 btnTranslate.addEventListener("click",clickHandker);
